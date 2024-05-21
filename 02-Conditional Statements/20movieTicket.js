7a13ea34112fce5ce245d5a9bb02c311c19987a4
Petar Velikov <pepo_velikov@mail.bg>
2024-05-21 14:01:32 +0300

    20.Movie ticket

---
+++ 02-Conditional Statements/20movieTicket.js
@@ -0,0 +1,12 @@
+function movieTicket(age) {
+
+  if (age > 0 && age <= 12) {
+    console.log('$5');
+  } else if (age >=13 && age <= 19) {
+    console.log('$8');
+  } else if (age >= 20) {
+    console.log('$10');
+  }
+}
+
+movieTicket(13);
