a8db17947be8d863c52cddce86854d96ce4382f0
Petar Velikov <pepo_velikov@mail.bg>
2024-05-21 22:35:28 +0300

    29.Climate zone identifier

---
+++ 02-Conditional Statements/29climateZone.js
@@ -0,0 +1,14 @@
+function climateZoneIdentifier(latitude, hemisphere) {
+
+  if (latitude > 66.5 || latitude < -66.5) {
+    console.log('Arctic Zone');
+  } else if (latitude >= 23.5 && latitude <= 66.5) {
+    console.log('Temperate Zone');
+  } else if (latitude > 0 && latitude < 23.5) {
+    console.log('Tropic Zone');
+  } else if (latitude == 0) {
+    console.log('Equator');
+  }
+}
+
+climateZoneIdentifier(0, 'S')
